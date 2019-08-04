d8663ade4a4dad78ec465260061faa4c096225f8
Piotr Hyriak <piotr1hyriak@gmail.com>
2019-08-04 09:42:24 +0200

    initial commit

---
+++ src/main/java/pl/sdaacademy/javapoz19programowanie1/Application.java
@@ -0,0 +1,10 @@
+package pl.sdaacademy.javapoz19programowanie1;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello World");
+    }
+}
