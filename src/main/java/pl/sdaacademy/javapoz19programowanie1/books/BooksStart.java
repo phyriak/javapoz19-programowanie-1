31ed43a6a594ac83f3d9ff17eacfe584d22dcd93
Piotr Hyriak <piotr1hyriak@gmail.com>
2019-08-04 14:51:31 +0200

    initial structure of aplication

---
+++ src/main/java/pl/sdaacademy/javapoz19programowanie1/books/BooksStart.java
@@ -0,0 +1,47 @@
+package pl.sdaacademy.javapoz19programowanie1.books;
+
+import java.util.Scanner;
+
+public class BooksStart {
+
+
+    private BooksViews views;
+
+    public BooksStart() {
+
+        this.views=new BooksViews(new Scanner(System.in));
+    }
+
+
+    public void start (){
+        boolean flag =true;
+        do {
+            int decision = views.startMenu();
+            switch (decision){
+                case 1:
+                       authorsView();
+                       break;
+                case 2:
+                        booksView();
+                        break;
+                default:
+                    flag=false;
+
+
+            }
+
+
+        }while (flag);
+
+
+
+    }
+
+    private void authorsView() {
+        System.out.println("Tutaj bedą autorzy");
+    }
+
+    private void booksView() {
+        System.out.println("Tutaj bedą ksiazki");
+    }
+}
---
+++ src/main/java/pl/sdaacademy/javapoz19programowanie1/books/BooksViews.java
@@ -0,0 +1,23 @@
+package pl.sdaacademy.javapoz19programowanie1.books;
+
+import java.util.Scanner;
+
+public class BooksViews {
+    private Scanner scanner;
+
+    public BooksViews(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public int startMenu(){
+        System.out.println("1. Authors");
+        System.out.println("2. Books");
+        System.out.println("0. Koniec");
+        int menu = scanner.nextInt();
+        scanner.nextLine();
+        return menu;
+
+    }
+
+
+}
