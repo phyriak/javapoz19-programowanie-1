ca382a2e10f5ef2437dc1d8ffebcfd867a8b9ee0
Piotr Hyriak <piotr1hyriak@gmail.com>
2019-08-31 11:52:38 +0200

    Commit list

--- src/main/java/lists/MyLinkedList.java
+++ src/main/java/lists/MyLinkedList.java
@@ -6,7 +6,6 @@ public class MyLinkedList {
     private int size;
 
     public MyLinkedList() {
-
         this.head = null;
         this.size = 0;
     }
@@ -14,50 +13,85 @@ public class MyLinkedList {
     public void add(Integer value) {
         MyLinkedListItem myLinkedListItem = new MyLinkedListItem(value);
 
-
         if (this.head == null) {
             this.head = myLinkedListItem;
         } else {
             MyLinkedListItem lastItem = getLastItem();
             lastItem.nextItem = myLinkedListItem;
         }
+
         size++;
-
-
     }
-    //TODO obsluga pustej listy
-    //TODO obsluga blednego indexu(ArrayIndexOutOfBound)
+
+
+
+    // TODO - obsluga pustej listy
+    // TODO - obluga blednego indexu (ArrayIndexOutOfBoundException)
+    public Integer get(int index) {
+        return getElement(index).value;
+    }
+
+    private MyLinkedListItem getElement(int index) {
+//1.Obsługa błednego indexu
+        if (index >= size) {
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+        MyLinkedListItem tmpItem = head;
+        for (int i = 0; i < index; i++) {
+            tmpItem = tmpItem.nextItem;
+        }
+        return tmpItem;
+    }
+
+    public void remove(int index) {
+        // bedzie trzeba uzyc metody getElement
+
+        // 1. pobieramy poprzednik elementu index
+        MyLinkedListItem previous = getElement(index - 1);
+
+        // 2. pobieramy element do usunieca
+        MyLinkedListItem toRemove = getElement(index);
+
+        // 3. zmieniamy nextValue w poprzedniku na nextValue elementu ktory usuwamy
+        previous.nextItem = toRemove.nextItem;
+//        previous.nextItem = previous.nextItem.nextItem;
+
+        // 4. w usuwanym elemencie null'ujemy nextValue
+        toRemove.nextItem = null;
+
+        size--;
+    }
+
+    // TODO - na 31.08 zadanie domowe
+    public void add(int index, Integer value) {
+
+        //1.Tworzymy nowe pudełeczko
+        MyLinkedListItem toAdd = new MyLinkedListItem(value);
+        //2 Pobieramy strzałke z poprzedniego pudełeczka do nastęnego
+        MyLinkedListItem previous = getElement(index - 1);
+        //3. Przypisujemy tą strzałkę nowemu pudełku tak aby był powiązany z nastepnym
+        toAdd.nextItem = previous.nextItem;
+        //4. Nadajemy nową strzałkę poprzedającemu pudełku, która wskazuje na nowy dodany obiekt
+        previous.nextItem = toAdd;
+        //5.Zwiększamy tablice
+        size++;
+    }
 
     public int size() {
         return size;
     }
 
-    public Integer get (int index){
-
-        MyLinkedListItem tmpItem=head;
-        for (int i = 1; i <index ; i++) {
-          tmpItem=tmpItem.nextItem;
-        }
-        return tmpItem.value;
-    }
-
     private MyLinkedListItem getLastItem() {
-
         MyLinkedListItem tmpItem = this.head;
         while (tmpItem.nextItem != null) {
             tmpItem = tmpItem.nextItem;
-
-
         }
         return tmpItem;
-
     }
 
     static class MyLinkedListItem {
-
         private Integer value;
         private MyLinkedListItem nextItem;
-
 
         private MyLinkedListItem(Integer value) {
             this.value = value;
--- src/test/java/lists/MyLinkedListTest.java
+++ src/test/java/lists/MyLinkedListTest.java
@@ -50,7 +50,6 @@ public class MyLinkedListTest {
         MyLinkedList myLinkedList = new MyLinkedList();
 
 
-
         myLinkedList.add(0);
         myLinkedList.add(10);
         myLinkedList.add(20);
@@ -61,23 +60,96 @@ public class MyLinkedListTest {
 
         //when
 
-        Integer result =myLinkedList.get(3);
+        Integer result = myLinkedList.get(3);
 
         //then
-        Assert.assertEquals((Integer)20,result);
+        Assert.assertEquals((Integer) 30, result);
 
     }
 
 
     @Test
-    public void getShouldReturnFirstElement(){
+    public void getShouldReturnFirstElement() {
 //TODO dopisac testy
 
     }
 
     @Test
-    public void getShouldReturnLastElement(){
+    public void getShouldReturnLastElement() {
 //TODO dopisac testy
+
+    }
+
+    @Test
+    public void removeShouldRemoveMiddleElement() {
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+
+        myLinkedList.add(0);
+        myLinkedList.add(10);
+        myLinkedList.add(20);
+
+        myLinkedList.add(30);
+        myLinkedList.add(40);
+
+
+        myLinkedList.remove(4);
+
+
+        Assert.assertEquals(4, myLinkedList.size());
+        Assert.assertEquals((Integer) 0, myLinkedList.get(0));
+        Assert.assertEquals((Integer) 10, myLinkedList.get(1));
+        Assert.assertEquals((Integer) 20, myLinkedList.get(2));
+        Assert.assertEquals((Integer) 30, myLinkedList.get(3));
+
+
+    }
+
+    @Test
+    public void addShouldAddMiddleElement() {
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+
+        myLinkedList.add(0);
+        myLinkedList.add(10);
+
+        myLinkedList.add(30);
+        myLinkedList.add(40);
+        myLinkedList.add(50);
+
+
+        myLinkedList.add(2, 20);
+
+
+        Assert.assertEquals(6, myLinkedList.size());
+        Assert.assertEquals((Integer) 0, myLinkedList.get(0));
+        Assert.assertEquals((Integer) 10, myLinkedList.get(1));
+        Assert.assertEquals((Integer) 20, myLinkedList.get(2));
+        Assert.assertEquals((Integer) 30, myLinkedList.get(3));
+        Assert.assertEquals((Integer) 40, myLinkedList.get(4));
+        Assert.assertEquals((Integer) 50, myLinkedList.get(5));
+
+
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void get_ElementOutOfListShouldThrowExceptionWhenAddingOnWrongIndex() {
+        // given
+        MyLinkedList myLinkedList = new MyLinkedList();
+
+
+        myLinkedList.add(0);
+        myLinkedList.add(10);
+
+        myLinkedList.add(30);
+        myLinkedList.add(40);
+        myLinkedList.add(50);
+        //when
+
+        myLinkedList.get(56);
+
 
     }
 }
